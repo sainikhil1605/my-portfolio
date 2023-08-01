baaf69e7c0463de8dfd7d16e394aa9b37fe7723c
Nikhil Reddy <35322884+sainikhil1605@users.noreply.github.com>
2023-08-01 11:59:55 -0500

    Fixed header and footer

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, lazy, Suspense} from "react";
 import {openSource} from "../../portfolio";
 import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
-
+import response from "../../assets/profile.json";
 const renderLoader = () => <Loading />;
 const GithubProfileCard = lazy(() =>
   import("../../components/githubProfileCard/GithubProfileCard")
@@ -16,22 +16,22 @@ export default function Profile() {
   useEffect(() => {
     if (openSource.showGithubProfile === "true") {
       const getProfileData = () => {
-        fetch("/profile.json")
-          .then(result => {
-            if (result.ok) {
-              return result.json();
-            }
-          })
-          .then(response => {
-            setProfileFunction(response.data.user);
-          })
-          .catch(function (error) {
-            console.error(
-              `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
-            );
-            setProfileFunction("Error");
-            openSource.showGithubProfile = "false";
-          });
+        // fetch("./profile.json")
+        //   .then(result => {
+        //     if (result.ok) {
+        //       return result.json();
+        //     }
+        //   })
+        //   .then(response => {
+        setProfileFunction(response.data.user);
+        // })
+        // .catch(function (error) {
+        //   console.error(
+        //     `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
+        //   );
+        //   setProfileFunction("Error");
+        //   openSource.showGithubProfile = "false";
+        // });
       };
       getProfileData();
     }

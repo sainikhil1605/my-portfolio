c2667bb349dc32a69cd3151a3a0b70a8c2d66b71
sainikhil1605 <sainikhilvatti1605@gmail.com>
2024-03-10 16:56:16 -0500

    Fixed image for apply ease

--- src/portfolio.js
+++ src/portfolio.js
@@ -258,7 +258,7 @@ const mobileApps = {
 const chromeExtensions = {
   projects: [
     {
-      image: require("./assets/images/apply-ease.jpeg"),
+      image: require("./assets/images/apply-ease.jpg"),
       projectName: "Apply Ease",
       projectDesc:
         "A chrome extension that helps users to apply for multiple jobs at once by filling the common details in the job application form.",
